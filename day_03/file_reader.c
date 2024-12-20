257a08bc68a6aa53f4e0220fcc08791ec806b7f4
l.serra <l.serra@white3.it>
2024-12-20 12:27:45 +0100

    day 3 solution

--- day_03/file_reader.c
+++ day_03/file_reader.c
@@ -1,79 +1,28 @@
 #include "file_reader.h"
 
-void add_char_to_text(char* text, char c, int size)
-{
-}
-
-data read_file(char* filename)
-{
-    FILE* input = fopen(filename, "r");
-
-    data result;
-    char* text;
-    int count = 0;
-
-    if(input != NULL)
-    {
-        char ch;
-        while ((ch=fgetc(input)) != EOF)
-        {
-            printf("[%d] %c\n", count, ch);
-            if(count==0)
-            {
-                text = (char*)malloc(sizeof(char));
-            }
-            else
-            {
-                text = (char*)realloc(text, count * sizeof(char));
-            }
-            text[count] = ch;
-            count++;
-            // add_char_to_text(text, ch, length);
-        }
-
-        fclose(input);
-    }
-    else
-    {
-        fprintf(stderr, "Unable to open the file!\n");
-    }
-
-
-    result.text = text;
-    result.length = count;
-
-    return result;
-}
-
-
 char* read_text(char* filename)
 {
     FILE* input = fopen(filename, "r");
 
-    char* line = (char*)malloc(LINE_LEN*sizeof(char));
-    char* text = (char*)malloc(1024*sizeof(char));
+    char* text = 0;
+    long length;
 
-    // memset(text, 0, 1024*sizeof(char));
-
-    int count = 0;
-
-    if(input != NULL)
+    if(input)
     {
-        char ch;
-        while (fgets(line, LINE_LEN, input))
+        fseek(input, 0, SEEK_END);
+        length = ftell(input);
+        fseek(input, 0, SEEK_SET);
+        text = (char*)malloc(length*sizeof(char));
+        if(text)
         {
-            printf("%s\n", line);
-            strcmp(text, line);
+            fread(text, 1, length, input);
         }
-
         fclose(input);
     }
     else
     {
         fprintf(stderr, "Unable to open the file!\n");
     }
-
-    // printf("text: %s\n", text);
 
     return text;
 }
--- day_03/main.c
+++ day_03/main.c
@@ -23,16 +23,13 @@ int main(int argc, char* argv[])
     printf("read file: '%s'\n", filename);
     // data d = read_file(filename);
     char* t = read_text(filename);
-    printf("\ntext: %s\n", t);
-
-    // printf("\ntext: %s\n", d.text);
-    // printf("\nlength: %d\n", d.length);
+    // printf("\nreturned text: %s\n", t);
 
     // char* t = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     // int result = parse_string(d.text, d.length);
     int result = parse_string(t);
 
-    // printf("result: %d\n", result);
+    printf("result: %d\n", result);
 
     printf("\n");
     return 0;
